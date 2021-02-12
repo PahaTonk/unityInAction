6e12fd3084d8182902353464addf9fff7e887456
Pavel Tankovich <gavrikboss@gmail.com>
2021-02-13 01:29:19 +0300

    Scene controller

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+  [SerializeField] private GameObject enemyPrefab;
+  private GameObject _enemy;
+
+  void Update()
+  {
+    if (_enemy != null) return;
+
+    _enemy = Instantiate(enemyPrefab);
+    float randomAngle = Random.Range(0, 360);
+
+    _enemy.transform.position = new Vector3(0, 1, 0);
+    _enemy.transform.Rotate(0, randomAngle, 0);
+  }
+}
